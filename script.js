ff9578b20eec5a50fb49ed92de15ab277036bfad
Zeus Yousif <zues-yousif@west-mec.org>
2015-10-08 16:58:11 -0700

    no message

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ google.setOnLoadCallback(drawMultSeries);
 
 function drawMultSeries() {
     var data = google.visualization.arrayToDataTable([
-        ['Food', '2011 Foods', '2000 Foods '],
+        ['Food', '2011 Food Sales', '2000 Food Sales '],
         ['Beans, ', 8173400, 83408000],
         ['Tortilla, ', 3792000, 3344000],
         ['Cheese ', 2695000, 3896000],
@@ -15,15 +15,16 @@ function drawMultSeries() {
     ]);
 
     var options = {
-        title: 'Most popular foods amoungst americans',
+        title: 'Most popular foods among americans',
         chartArea: {width: '50%'},
         hAxis: {
-            title: 'Total Population',
+            title: 'Total Sales in US',
             minValue: 0
         },
         vAxis: {
-            title: 'City'
-        }
+            title: 'Foods'
+        },
+        backgroundColor:"grey"
     };
 
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
